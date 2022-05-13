ff3035fd1852314676d6cd83a2b302ddf4a72e89
lancao2 <lancao.af@gmail.com>
2022-05-13 00:30:08 -0300

    style: vetor component

---
+++ src/components/Vetor/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function Vetor() {
+  return (
+    <div className="vetor">
+      <div className="box1"></div>
+      <div className="box2"></div>
+      <h1>O primeiro passo para o futuro começa aqui !</h1>
+    </div>
+  );
+}
