f86a7c2b55ea3c4625ee00bb4034e05c04d70c0e
Ícaro Nascimento <ikaro_460@hotmail.com>
2022-05-16 10:46:30 -0300

    fix: Login - Toast component incorporado

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,12 +1,17 @@
-import { Checkbox, FormControlLabel, FormGroup, TextField, Typography, useMediaQuery } from "@mui/material";
+import {
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  TextField,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 import {
   BoxForm,
   BoxPadlock,
   BoxSingup,
   BoxSingupPadlock,
   ButtonSubmit,
-  CheckboxGroup,
-  DivCheckBox,
   LinkToLogin,
   LoginScreen,
 } from "./styles.jsx";
@@ -21,6 +26,7 @@ import LoginBackground from "../../imgs/Asset 1.png";
 import { makeStyles } from "@mui/styles";
 import { Link } from "react-router-dom";
 import { api } from "../../services/api";
+import { ToastError, ToastSuccess } from "../../components/Toasts/Index.jsx";
 
 const useStyles = makeStyles((themes) => ({
   img: {
@@ -70,9 +76,9 @@ export default function Login() {
         localStorage.setItem("userId", id);
 
         setAuthenticated(true);
-        toast.success("O login foi um sucesso");
+        ToastSuccess("O login foi um sucesso");
       })
-      .catch((err) => toast.error("Email ou senha inválidos"));
+      .catch((err) => ToastError("Email ou senha inválidos"));
   };
 
   const isActive = useMediaQuery(`(min-width:800px)`);
@@ -89,9 +95,11 @@ export default function Login() {
       <BoxSingup>
         <BoxSingupPadlock>
           <BoxPadlock>
-            <LockOutlinedIcon sx={{color: "text.secondary"}} />
+            <LockOutlinedIcon sx={{ color: "text.secondary" }} />
           </BoxPadlock>
-          <Typography variant="h4" color="text.primary" >Login</Typography>
+          <Typography variant="h4" color="text.primary">
+            Login
+          </Typography>
         </BoxSingupPadlock>
         <BoxForm onSubmit={handleSubmit(onSubmitFunction)}>
           <TextField
@@ -112,7 +120,12 @@ export default function Login() {
             type="password"
           />
           <FormGroup>
-            <FormControlLabel control={<Checkbox defaultChecked />} label={<Typography variant="h5" >Mantenha-me conectado</Typography> } />
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label={
+                <Typography variant="h5">Mantenha-me conectado</Typography>
+              }
+            />
           </FormGroup>
           <ButtonSubmit variant="contained" type="submit">
             Entrar
