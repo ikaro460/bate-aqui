5913fee78e38d3ca25eb797dbd9d22c79f0e451c
yuridsb <yuriboeno@hotmail.com>
2022-05-16 19:25:47 -0300

    fix: hourProvider inside provider/index.jsx

--- src/provider/index.jsx
+++ src/provider/index.jsx
@@ -1,22 +1,21 @@
-import { DarkModeProvider } from "./DarkMode"
-import { SideBarProvider } from "./SideBar"
-import { OpenModalCreateGroupProvider } from "./OpenModalCreateGroup"
-import { UsersProvider } from "./Users"
-import { OwnerGroupsProvider } from "./OwnerGroups"
-export const Providers = ({children}) => {
-
-  return(
-    <DarkModeProvider>
-      <SideBarProvider>
-        <OpenModalCreateGroupProvider>
-          <UsersProvider>
-            <OwnerGroupsProvider>
-              {children}
-            </OwnerGroupsProvider>      
-          </UsersProvider>   
-        </OpenModalCreateGroupProvider>     
-      </SideBarProvider>
-    </DarkModeProvider>
-  )
-
-}
+import { DarkModeProvider } from "./DarkMode";
+import { SideBarProvider } from "./SideBar";
+import { OpenModalCreateGroupProvider } from "./OpenModalCreateGroup";
+import { UsersProvider } from "./Users";
+import { OwnerGroupsProvider } from "./OwnerGroups";
+import { HourProvider } from "./Hour";
+export const Providers = ({ children }) => {
+    return (
+        <DarkModeProvider>
+            <SideBarProvider>
+                <OpenModalCreateGroupProvider>
+                    <UsersProvider>
+                        <OwnerGroupsProvider>
+                            <HourProvider>{children}</HourProvider>
+                        </OwnerGroupsProvider>
+                    </UsersProvider>
+                </OpenModalCreateGroupProvider>
+            </SideBarProvider>
+        </DarkModeProvider>
+    );
+};
