ab85676d57cf1382edc5f0563c3a883455728a08
igorscotton <scotton.igor@gmail.com>
2022-05-16 16:02:36 -0300

    provider coach groups created and api requisition implemented

---
+++ src/provider/CoachGroups/index.js
@@ -0,0 +1,25 @@
+import { createContext, useState, useContext } from "react";
+import { api } from "../../services/api";
+
+const CoachGroupsContext = createContext([]);
+
+export const CoachGroupsProvider = ({children}) => {
+
+    const [coachGroups, setCoachGroups] = useState([]);
+
+    const getCoachGroups = (token, id) => {
+        api.get(`/users/${id}/?_embed=coach`, {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }            
+        }).then((response) => setCoachGroups(response.data.coach)).catch((error) => console.log(error))    
+    }
+    
+    return (
+        <CoachGroupsContext.Provider value={{coachGroups, setCoachGroups, getCoachGroups}}>
+            {children}
+        </CoachGroupsContext.Provider>
+    )
+}
+
+export const useCoachGroups = () => useContext(CoachGroupsContext)
