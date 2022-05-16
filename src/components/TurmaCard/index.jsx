686809e4de25fa59f60f01f9d8b54a0007190e0d
IgorTenoso <lucasmirandaneves@gmail.com>
2022-05-16 11:32:25 -0300

    feat: dropdown menu on MoreVert

--- src/components/TurmaCard/index.jsx
+++ src/components/TurmaCard/index.jsx
@@ -1,18 +1,41 @@
-import { Box, Button, IconButton, Stack, Typography } from "@mui/material"
+import { Box, Button, IconButton, Menu, MenuItem, Stack, Typography } from "@mui/material"
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { ColorCard, InfosCard, StyledCard } from "./styles";
+import { useState } from "react";
 
 
 export default function TurmaCard() {
+
+  const [ openMore, setOpenMore ] = useState(false)
+  const [anchorEl, setAnchorEl]   = useState(null);
+
+  const toggleMore = (event) => {
+    setOpenMore(!openMore)
+    setAnchorEl(event.currentTarget)
+  }
 
   return(
     <StyledCard >
 
       <ColorCard>
 
-        <IconButton sx={{position: "absolute", top: "0", right: "0"}} >
+        <IconButton id="basic-button" onClick={toggleMore} aria-controls={openMore ? 'basic-menu' : undefined} aria-haspopup="true" aria-expanded={openMore ? 'true' : undefined} sx={{position: "absolute", top: "0", right: "0"}} >
           <MoreVertIcon sx={{color: "background.primary"}} />
         </IconButton>
+
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={openMore}
+          onClose={toggleMore}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+        >
+          <MenuItem>Editar</MenuItem>
+          <MenuItem>Excluir</MenuItem>
+        </Menu>
 
       </ColorCard>
 
