7efb81d3ad59bf35ec489c73e48f330a174a5140
yuridsb <yuriboeno@hotmail.com>
2022-05-17 23:01:17 -0300

    feat: table info in /turma

--- src/components/StyledTableRow/index.jsx
+++ src/components/StyledTableRow/index.jsx
@@ -1,64 +1,62 @@
-import { Box, Button, Card, CardMedia, TableContainer, Typography, Paper, Table, TableHead, TableRow, TableCell, TableBody, IconButton, ButtonGroup } from "@mui/material"
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+import {
+    Box,
+    Button,
+    Card,
+    CardMedia,
+    TableContainer,
+    Typography,
+    Paper,
+    Table,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody,
+    IconButton,
+    ButtonGroup,
+} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 
-export default function StyledTableRow({row, index}) {
+export default function StyledTableRow({ row, index }) {
+    const { id, name, data, checkin, checkout, students, works, problems } = row;
 
-  const {id, name, data, checkin, checkout, status} = row
-
-  return(
-    <TableRow
-      sx={[
-
-        index % 2 === 0 && {
-          backgroundColor: "tableRowContrast"
-        },
-
-      ]}
-    >
-      <TableCell
-        component="th"
-        scope="row"
-        sx={{color: "text.primary", textAlign: "center"}}
-      >
-        <Typography variant="tableSubTitle" >{id}</Typography>
-      </TableCell>
-      <TableCell
-        component="th"
-        scope="row"
-        sx={{color: "text.primary"}}
-      >
-        <Typography variant="tableSubTitle" >{name}</Typography>
-      </TableCell>
-      <TableCell
-        component="th"
-        scope="row"
-        sx={{color: "text.primary"}}
-      >
-        <Typography variant="tableSubTitle" >{data}</Typography>
-      </TableCell>
-      <TableCell
-        component="th"
-        scope="row"
-        sx={{color: "text.primary"}}
-      >
-        <Typography variant="tableSubTitle" >{checkin}</Typography>
-      </TableCell>
-      <TableCell
-        component="th"
-        scope="row"
-        sx={{color: "text.primary"}}
-      >
-        <Typography variant="tableSubTitle" >{checkout}</Typography>
-      </TableCell>
-      <TableCell
-        component="th"
-        scope="row"
-        sx={{color: "text.primary"}}
-      >
-        <Typography variant="tableSubTitle" >{status}</Typography>
-      </TableCell>
-      <TableCell
+    return (
+        <TableRow
+            sx={[
+                index % 2 === 0 && {
+                    backgroundColor: "tableRowContrast",
+                },
+            ]}
+        >
+            <TableCell
+                component="th"
+                scope="row"
+                sx={{ color: "text.primary", textAlign: "center" }}
+            >
+                <Typography variant="tableSubTitle">{id}</Typography>
+            </TableCell>
+            <TableCell component="th" scope="row" sx={{ color: "text.primary" }}>
+                <Typography variant="tableSubTitle">{name}</Typography>
+            </TableCell>
+            <TableCell component="th" scope="row" sx={{ color: "text.primary" }}>
+                <Typography variant="tableSubTitle">{data}</Typography>
+            </TableCell>
+            <TableCell component="th" scope="row" sx={{ color: "text.primary" }}>
+                <Typography variant="tableSubTitle">{checkin}</Typography>
+            </TableCell>
+            <TableCell component="th" scope="row" sx={{ color: "text.primary" }}>
+                <Typography variant="tableSubTitle">{checkout}</Typography>
+            </TableCell>
+            <TableCell component="th" scope="row" sx={{ color: "text.primary", maxWidth: 100 }}>
+                <Typography variant="tableSubTitle">{students}</Typography>
+            </TableCell>
+            <TableCell component="th" scope="row" sx={{ color: "text.primary", maxWidth: 100 }}>
+                <Typography variant="tableSubTitle">{works}</Typography>
+            </TableCell>
+            <TableCell component="th" scope="row" sx={{ color: "text.primary", maxWidth: 100 }}>
+                <Typography variant="tableSubTitle">{problems}</Typography>
+            </TableCell>
+            {/* <TableCell
         component="th"
         scope="row"
         sx={{color: "text.primary", textAlign: "center", width: "140px"}}
@@ -71,8 +69,7 @@ export default function StyledTableRow({row, index}) {
             <DeleteOutlinedIcon sx={{color: "secondary.main"}} />
           </IconButton>
         </ButtonGroup>
-      </TableCell>
-    </TableRow>
-  )
-
+      </TableCell> */}
+        </TableRow>
+    );
 }
--- src/pages/Turma/index.jsx
+++ src/pages/Turma/index.jsx
@@ -1,98 +1,252 @@
-import { Box, Button, Card, CardMedia, TableContainer, Typography, Paper, Table, TableHead, TableRow, TableCell, TableBody, Stack } from "@mui/material"
-import { styled } from '@mui/material/styles';
-import { makeStyles } from "@mui/styles"
+import {
+    Box,
+    Button,
+    Card,
+    CardMedia,
+    TableContainer,
+    Typography,
+    Paper,
+    Table,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody,
+    Stack,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { makeStyles } from "@mui/styles";
 import StyledTableRow from "../../components/StyledTableRow";
-import AddSharpIcon from '@mui/icons-material/AddSharp';
+import AddSharpIcon from "@mui/icons-material/AddSharp";
+import { api } from "../../services/api";
+import { useEffect, useState } from "react";
 
+const useStyles = makeStyles((themes) => ({
+    profile: {
+        position: "absolute",
+        top: "0px",
+    },
+}));
 
-const useStyles = makeStyles( (themes) => ({
+const ContainerBox = styled(Box)(({ theme }) => ({
+    minWidth: "max-content",
+    minHeight: "100vh",
 
-  profile: {
-    position: "absolute",
-    top: "0px",
-  }
+    textAlign: "left",
+    position: "relative",
 
-}))
+    padding: "25px 30px",
+    backgroundColor: theme.palette.background.primary,
+}));
 
-const ContainerBox = styled(Box)(({theme}) => ({
-  minWidth: "max-content", 
-  minHeight: "100vh",
-
-  textAlign: "left",
-  position: "relative",
-
-  padding: "25px 30px",
-  backgroundColor: theme.palette.background.primary,
-}))
-
-
-function createData(id, name, data, checkin, checkout, status) {
-  return {id, name, data, checkin, checkout, status};
+function createData(id, name, data, checkin, checkout, students, works, problems) {
+    return { id, name, data, checkin, checkout, students, works, problems };
 }
 
-const rows = [
-  createData(5, "Jorge", "12/02/2022", "11:00", "14:00", "verdin"),
-  createData(6, "Yuri", "12/02/2022", "11:05", "14:11", "verdin"),
-  createData(7, "Icaro", "12/02/2022", "11:20", "14:00", "atrasado"),
-  createData(8, "Alex", "12/02/2022", "11:00", "14:00", "verdin"),
-  createData(9, "Igor", "12/02/2022", "11:00", "14:00", "verdin"),
-  createData(10, "Lucas", "12/02/2022", "11:00", "14:00", "verdin")
-]
+// const rows = [
+//     createData(5, "Jorge", "12/02/2022", "11:00", "14:00", "verdin"),
+//     createData(6, "Yuri", "12/02/2022", "11:05", "14:11", "verdin"),
+//     createData(7, "Icaro", "12/02/2022", "11:20", "14:00", "atrasado"),
+//     createData(8, "Alex", "12/02/2022", "11:00", "14:00", "verdin"),
+//     createData(9, "Igor", "12/02/2022", "11:00", "14:00", "verdin"),
+//     createData(10, "Lucas", "12/02/2022", "11:00", "14:00", "verdin"),
+// ];
+
+// console.log(rows);
 
 export default function Turma() {
+    const classes = useStyles();
 
-  const classes = useStyles()
+    const [groupCoachs, setGroupCoachs] = useState([]);
+    const [groupInfo, setGroupInfo] = useState([]);
+    const [groupCreatorName, setGroupCreatorName] = useState("");
+    const [infoCheckinAdmin, setInfoCheckinAdmin] = useState([]);
+    const [infoCheckinCoach, setInfoCheckinCoach] = useState([]);
 
-  return(
-    <ContainerBox>
+    const token = localStorage.getItem("accessToken");
+    const idUserLogged = localStorage.getItem("userId");
 
-      <Stack direction="row" alignItems="baseline" justifyContent="space-between" >
-        <Typography color="text.primary" variant="h3" >Turma 01</Typography>
-        <Button variant="contained" startIcon={<AddSharpIcon />} >Adicionar *Aluno*</Button>
-      </Stack>
-        
-      <Typography variant="subtitle1" ml="5px" color="text.subtitle" mb="50px" >Coach</Typography>
+    const groupsId = 1;
 
-      <TableContainer 
-        component={Paper} 
-        sx={{minWidth: "720px"}}
-      >
-        <Table sx={{overflowX: "scroll", minWidth: "max-content"}} >
-          <TableHead >
-            <TableRow >
-              <TableCell sx={{color: "text.primary", textAlign: "center"}} >
-                <Typography variant="tableTitle" >ID Aluno</Typography>
-              </TableCell>
-              <TableCell sx={{color: "text.primary"}} >
-                <Typography variant="tableTitle" >Nome</Typography>
-              </TableCell>
-              <TableCell sx={{color: "text.primary"}} >
-                <Typography variant="tableTitle" >Data</Typography>
-              </TableCell>
-              <TableCell sx={{color: "text.primary"}} >
-                <Typography variant="tableTitle" >Check-in</Typography>
-              </TableCell>
-              <TableCell sx={{color: "text.primary"}} >
-                <Typography variant="tableTitle" >Check-out</Typography>
-              </TableCell>
-              <TableCell sx={{color: "text.primary"}} >
-                <Typography variant="tableTitle" >Status</Typography>
-              </TableCell>
-              <TableCell sx={{color: "text.primary", textAlign: "center"}} >
-                <Typography variant="tableTitle" >Ações</Typography>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row, index) => 
-              <StyledTableRow row={row} key={index} index={index} />
+    useEffect(() => {
+        api.get(`groups/${groupsId}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then((res) => setGroupInfo(res.data))
+            .catch((err) => {
+                console.log(err);
+            });
 
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
+        api.get(`/coach?groupsId=${groupsId}`)
+            .then((res) => setGroupCoachs(res.data))
+            .catch((err) => {
+                console.log(err);
+            });
 
-    </ContainerBox>
-  )
+        api.get(`/checkin?groupsId=${groupsId}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then((res) => setInfoCheckinAdmin(res.data))
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
+    useEffect(() => {
+        const coachIdFind = groupCoachs.find((coach) => coach.userId == idUserLogged)?.id;
+
+        api.get(`/checkin?coachId=${coachIdFind}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then((res) => setInfoCheckinCoach(res.data))
+            .catch((err) => {
+                console.log(err);
+            });
+    }, [groupCoachs]);
+
+    useEffect(() => {
+        groupInfo.userId &&
+            api
+                .get(`/users/${groupInfo.userId}`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                })
+                .then((res) => setGroupCreatorName(res.data.name + " " + res.data.surname))
+                .catch((err) => {
+                    console.log(err);
+                });
+    }, [groupInfo]);
+
+    const rowsCheckin = [];
+
+    if (groupInfo.userId == idUserLogged) {
+        infoCheckinAdmin.forEach((checkin) => {
+            if (checkin.type === "checkin") {
+                rowsCheckin.push(
+                    createData(
+                        checkin.userId,
+                        checkin.name,
+                        checkin.date,
+                        checkin.hour,
+                        "",
+                        "",
+                        "",
+                        ""
+                    )
+                );
+            } else
+                rowsCheckin.push(
+                    createData(
+                        checkin.userId,
+                        checkin.name,
+                        checkin.date,
+                        "",
+                        checkin.hour,
+                        checkin.students,
+                        checkin.works,
+                        checkin.problems
+                    )
+                );
+        });
+    } else {
+        infoCheckinCoach.forEach((checkin) => {
+            if (checkin.type === "checkin") {
+                rowsCheckin.push(
+                    createData(
+                        checkin.userId,
+                        checkin.name,
+                        checkin.date,
+                        checkin.hour,
+                        "",
+                        "",
+                        "",
+                        ""
+                    )
+                );
+            } else
+                rowsCheckin.push(
+                    createData(
+                        checkin.userId,
+                        checkin.name,
+                        checkin.date,
+                        "",
+                        checkin.hour,
+                        checkin.students,
+                        checkin.works,
+                        checkin.problems
+                    )
+                );
+        });
+    }
+
+    return (
+        <ContainerBox>
+            <Stack direction="row" alignItems="baseline" justifyContent="space-between">
+                <Typography color="text.primary" variant="h3">
+                    {groupInfo.name}
+                </Typography>
+                <Button variant="contained" startIcon={<AddSharpIcon />}>
+                    Adicionar *Aluno*
+                </Button>
+            </Stack>
+            <Stack direction="row" alignItems="baseline" justifyContent="space-between">
+                {groupCreatorName && (
+                    <Typography variant="subtitle1" ml="5px" color="text.subtitle" mb="50px">
+                        Grupo criado por {groupCreatorName}!
+                    </Typography>
+                )}
+                <Button variant="contained">Voltar</Button>
+            </Stack>
+
+            <TableContainer
+                component={Paper}
+                sx={{ width: "100%", minWidth: 720, overflowX: "auto" }}
+            >
+                <Table sx={{ overflowX: "scroll", minWidth: "max-content" }}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell sx={{ color: "text.primary", textAlign: "center" }}>
+                                <Typography variant="tableTitle">ID Aluno</Typography>
+                            </TableCell>
+                            <TableCell sx={{ color: "text.primary" }}>
+                                <Typography variant="tableTitle">Nome</Typography>
+                            </TableCell>
+                            <TableCell sx={{ color: "text.primary" }}>
+                                <Typography variant="tableTitle">Data</Typography>
+                            </TableCell>
+                            <TableCell sx={{ color: "text.primary" }}>
+                                <Typography variant="tableTitle">Check-in</Typography>
+                            </TableCell>
+                            <TableCell sx={{ color: "text.primary" }}>
+                                <Typography variant="tableTitle">Check-out</Typography>
+                            </TableCell>
+                            <TableCell sx={{ color: "text.primary" }}>
+                                <Typography variant="tableTitle">Alunos Atendidos</Typography>
+                            </TableCell>
+                            <TableCell sx={{ color: "text.primary" }}>
+                                <Typography variant="tableTitle">Atividades Realizadas</Typography>
+                            </TableCell>
+                            <TableCell sx={{ color: "text.primary" }}>
+                                <Typography variant="tableTitle">Pendências</Typography>
+                            </TableCell>
+                            {/* <TableCell sx={{ color: "text.primary", textAlign: "center" }}>
+                                <Typography variant="tableTitle">Ações</Typography>
+                            </TableCell> */}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rowsCheckin.reverse().map((row, index) => (
+                            <StyledTableRow row={row} key={index} index={index} />
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </ContainerBox>
+    );
 }
