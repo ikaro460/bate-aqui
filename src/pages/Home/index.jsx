688e354f24b4ad31160b2502dad72d1a21884a0f
Ícaro Nascimento <ikaro_460@hotmail.com>
2022-05-19 22:25:50 -0300

    feat/Delete group

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,11 @@
-import { Modal, Button, Grid, Stack, Typography, Skeleton } from "@mui/material";
+import {
+  Modal,
+  Button,
+  Grid,
+  Stack,
+  Typography,
+  Skeleton,
+} from "@mui/material";
 import TurmaCard from "../../components/TurmaCard";
 import CreateGroupButton from "../../components/CreateGroupButton";
 import { useOpenModalCreateGroup } from "../../provider/OpenModalCreateGroup";
@@ -14,141 +21,148 @@ import ModalCheckout from "../../components/ModalCheckout";
 import TurmaCardCoach from "../../components/TurmaCardCoach";
 
 export default function Home() {
-    const { modalCreateGroup, toggleModalCreateGroup } = useOpenModalCreateGroup();
+  const { modalCreateGroup, toggleModalCreateGroup } =
+    useOpenModalCreateGroup();
 
-    const { modalNotification, toggleModalNotification } = useOpenModalNotification();
+  const { modalNotification, toggleModalNotification } =
+    useOpenModalNotification();
 
-    const { getCoachGroups, coachGroups, verifyNotify } = useCoachGroups();
+  const { getCoachGroups, coachGroups, verifyNotify } = useCoachGroups();
 
-    const [user, setUser] = useState(false);
+  const [user, setUser] = useState(false);
 
-    const [groups, setGroups] = useState(false);
+  const [groups, setGroups] = useState(false);
 
-    const { name, surname } = user;
+  const { name, surname } = user;
 
-    const { id } = useParams();
+  const { id } = useParams();
 
-    const history = useHistory();
+  const history = useHistory();
 
-    const axiosGetUser = () => {
-        api.get(`/users/${localStorage.getItem("userId")}`, {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-            },
-        })
-            .then((res) => {
-                setUser(res.data);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
-    const redirect = () => {
-        history.push(`/home/${localStorage.getItem("userId")} `);
-        axiosGetUser();
-    };
+  const axiosGetUser = () => {
+    api
+      .get(`/users/${localStorage.getItem("userId")}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+      .then((res) => {
+        setUser(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const redirect = () => {
+    history.push(`/home/${localStorage.getItem("userId")} `);
+    axiosGetUser();
+  };
 
-    useEffect(() => {
-        id === localStorage.getItem("userId") ? axiosGetUser() : redirect();
-    }, []);
+  useEffect(() => {
+    id === localStorage.getItem("userId") ? axiosGetUser() : redirect();
+  }, []);
 
-    useEffect(() => {
-        api.get(`/users/${localStorage.getItem("userId")}?_embed=groups`, {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-            },
-        })
-            .then((res) => {
-                setGroups(res.data.groups);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }, [modalCreateGroup]);
+  useEffect(() => {
+    api
+      .get(`/users/${localStorage.getItem("userId")}?_embed=groups`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+      .then((res) => {
+        setGroups(res.data.groups.filter((a) => a.status_ativo === 1));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [modalCreateGroup]);
 
-    useEffect(() => {
-        getCoachGroups(localStorage.getItem("accessToken"), id);
-    }, [verifyNotify]);
+  useEffect(() => {
+    getCoachGroups(localStorage.getItem("accessToken"), id);
+  }, [verifyNotify]);
 
-    return (
-        <ContainerBox>
-            <StyledCard elevation={2}>
-                <Typography variant="h5" sx={{ alignSelf: "flex-start", color: "text.primary" }}>
-                    Perfil
-                </Typography>
+  return (
+    <ContainerBox>
+      <StyledCard elevation={2}>
+        <Typography
+          variant="h5"
+          sx={{ alignSelf: "flex-start", color: "text.primary" }}
+        >
+          Perfil
+        </Typography>
 
-                <ProfileImg component="img" image={`https://robohash.org/${id}`} />
+        <ProfileImg component="img" image={`https://robohash.org/${id}`} />
 
-                <Stack
-                    direction="column"
-                    justifyContent="center"
-                    alignItems="center"
-                    spacing={1}
-                    sx={{ height: "100%" }}
-                >
-                    {user ? (
-                        <Typography variant="h5" sx={{ color: "text.primary" }}>
-                            {name} {surname}
-                        </Typography>
-                    ) : (
-                        <Skeleton variant="rectangular" height={21.34} width={110} />
-                    )}
+        <Stack
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          spacing={1}
+          sx={{ height: "100%" }}
+        >
+          {user ? (
+            <Typography variant="h5" sx={{ color: "text.primary" }}>
+              {name} {surname}
+            </Typography>
+          ) : (
+            <Skeleton variant="rectangular" height={21.34} width={110} />
+          )}
 
-                    <Typography variant="subtitle2" sx={{ color: "text.primary" }}>
-                        Instituição
-                    </Typography>
+          <Typography variant="subtitle2" sx={{ color: "text.primary" }}>
+            Instituição
+          </Typography>
 
-                    <Button variant="contained">Perfil</Button>
-                </Stack>
-            </StyledCard>
+          <Button variant="contained">Perfil</Button>
+        </Stack>
+      </StyledCard>
 
-            <StyledGrid container spacing={5}>
-                {groups &&
-                    groups.map((each, index) => (
-                        <Grid item key={index}>
-                            <TurmaCard group={each} type={"Facilitador"} />
-                        </Grid>
-                    ))}
+      <StyledGrid container spacing={5}>
+        {groups &&
+          groups.map((each, index) => (
+            <Grid item key={index}>
+              <TurmaCard group={each} type={"Facilitador"} />
+            </Grid>
+          ))}
 
-                {coachGroups &&
-                    coachGroups.map((each, index) => (
-                        <Grid item key={index}>
-                            <TurmaCardCoach group={each} type={"Coach"} />
-                        </Grid>
-                    ))}
+        {coachGroups &&
+          coachGroups.map((each, index) => (
+            <Grid item key={index}>
+              <TurmaCardCoach group={each} type={"Coach"} />
+            </Grid>
+          ))}
 
-                <Grid item>
-                    <CreateGroupButton />
-                </Grid>
-            </StyledGrid>
+        <Grid item>
+          <CreateGroupButton />
+        </Grid>
+      </StyledGrid>
 
-            <Modal
-                open={modalCreateGroup}
-                onClose={toggleModalCreateGroup}
-                sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    textAlign: "center",
-                }}
-            >
-                <ModalCreateGroup />
-            </Modal>
+      <Modal
+        open={modalCreateGroup}
+        onClose={toggleModalCreateGroup}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
+        <ModalCreateGroup />
+      </Modal>
 
-            <ModalCheckout useId={id} name={name} />
+      <ModalCheckout useId={id} name={name} />
 
-            <Modal
-                open={modalNotification}
-                onClose={toggleModalNotification}
-                sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    textAlign: "center",
-                }}
-            >
-                <ModalNotification></ModalNotification>
-            </Modal>
-        </ContainerBox>
-    );
+      <Modal
+        open={modalNotification}
+        onClose={toggleModalNotification}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
+        <ModalNotification></ModalNotification>
+      </Modal>
+    </ContainerBox>
+  );
 }
